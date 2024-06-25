7ae7bed0e1fd9e8b66fd5ceb8f206b1ffc6edb63
nahuel_dev <nanocode2022@gmail.com>
2024-06-25 14:02:33 -0300

    add:fetch blogs

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,13 +1,30 @@
 import { useEffect, useState } from 'react'
 import db from '../assets/db.json'
 export const useData = () => {
-  const [ users, setUsers ] = useState([])
-  const [ blogs, setBlogs ] = useState([])
+  const [users, setUsers] = useState([])
+  const [blogs, setBlogs] = useState([])
   const [tags, setTags] = useState([])
 
-  useEffect(()=>{
+  const get_blogs = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api');
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+      throw error;
+    }
+  };
+
+
+
+  useEffect(() => {
     setUsers(db.users)
-    setBlogs(db.blogPosts)
+    get_blogs().then(data => setBlogs(data))
+    // setBlogs(db.blogPosts)
     const tagsSet = new Set();
     db.blogPosts.forEach(post => {
       post.tags.forEach(tag => {
@@ -16,7 +33,7 @@ export const useData = () => {
     });
     const uniqueTags = Array.from(tagsSet);
     setTags(uniqueTags)
-  },[])
-   
-  return {users, blogs, tags}
+  }, [])
+
+  return { users, blogs, tags }
 }
